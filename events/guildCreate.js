1118b3d35b1ee4e8dc098490ae095de1c41cbcc8
BalenD <balen1996@hotmail.com>
2019-08-30 17:44:19 +0200

    added a welcome message event for member joining

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -4,16 +4,7 @@ class Guildcreate {
   }
 
   async run(guild) {
-    console.log('test');
-    console.log(guild);
-    console.log(this.client);
-    const settings = this.client.getSettings(guild.id);
-    // try {
-    // } catch (error) {
-    //   this.client.logger.error(error);
-    // }
-    const channel = this.client.channels.find(settings.welcomeChannel);
-    channel.send(settings.welcomeMessage);
+    //  TODO: add a welcome message
   }
 }
 
---
+++ events/guildMemberAdd.js
@@ -0,0 +1,20 @@
+class GuildMemberAdd {
+  constructor(client) {
+    this.client = client;
+  }
+
+  async run(guildMember) {
+    const settings = this.client.getSettings(guildMember.guild.id);
+    const channelToMessage = guildMember.guild.channels.find(channel => channel.name === settings.welcomeChannel);
+    console.log(settings.welcomeEnabled);
+    if (settings.welcomeEnabled === true) {
+      try {
+        channelToMessage.send(settings.welcomeMessage);
+      } catch (error) {
+        this.client.logger.error(error);
+      }
+    }
+  }
+}
+
+module.exports = GuildMemberAdd;

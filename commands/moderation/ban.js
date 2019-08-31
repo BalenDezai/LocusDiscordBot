d83b583a58b1b90767933f22100f8e51fd3d2600
BalenD <balen1996@hotmail.com>
2019-08-31 18:46:55 +0200

    fixed a minor bug in ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -8,17 +8,28 @@ class Ban extends Command {
       description: 'Bans a mentioned user or users, plus you can specify the amount of days of messages to remove',
       category: 'Moderation',
       usage: ['ban [user mention/s]', ['ban [user mention/s] -d [days] -r [reason]']],
+      guildOnly: true,
+      permLevel: ['Administrator', 'Moderator', 'Server Owner'],
     });
   }
 
   async run(message, args) {
+    // if (!message.guild) {
+    //   return;
+    // }
+
+    // if (senderRole.lvl < 1 || senderRole.lvl > 3) {
+    //   message.channel.send(Utils.createErrorMessage('You do not have permission to ban'));
+    //   return;
+    // }
+
     const indexOfD = args.indexOf('-d');
     const indexOfR = args.indexOf('-r');
     const days = parseInt(args.slice(indexOfD + 1, indexOfR), 10);
     const reason = args.slice(indexOfR + 1).join(' ');
     const bannedUsersTags = [];
 
-    if (message.mentions.users.length > 0) {
+    if (message.mentions.users.size > 0) {
       message.mentions.users.forEach((user) => {
         const member = message.guild.member(user);
         if (member) {
@@ -27,16 +38,15 @@ class Ban extends Command {
               reason,
               days,
             });
-  
             bannedUsersTags.push(user.tag);
           } catch (error) {
-            message.channel.send(Utils.createErrorMessage(`Failed to ban user: **${ user.tag }**`));
+            message.channel.send(Utils.createErrorMessage(`Failed to ban user: **${user.tag}**`));
             this.client.logger.error(error);
           }
         }
       });
-  
-      message.channel.send(Utils.createSuccessMessage(`Successfully banned user(s) ${ bannedUsersTags.join(' ') }`));
+
+      message.channel.send(Utils.createSuccessMessage(`Successfully banned user(s) ${bannedUsersTags.join(' ')}`));
     } else {
       message.channel.send(Utils.createErrorMessage('You didn\'t mention any users to ban'));
     }

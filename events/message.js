d9745a1a5549500160e161a1abb23de3b59e24df
Mac <ftrcsg@gmail.com>
2019-08-31 17:01:51 -0300

    Added a check and some more error handling in message.js

--- events/message.js
+++ events/message.js
@@ -34,24 +34,35 @@ class Message {
     // Prevent caching errors by fetching the member if for any reason they should be invisible or not cached
     if (message.guild && !message.member) await message.guild.fetchMember(message.author);
 
-    // Get the member's permission level
-    const lvlObject = this.client.permLevel(message);
-
     // Grab the command from the Collection we set earlier
     const command = this.client.commands.get(commandText) || this.client.commands.get(this.client.aliases.get(commandText));
     if (!command) return;
 
+    // Warn the user that the command is guild only
     if (!message.guild && command.conf.guildOnly) {
       return message.channel.send(Utils.createErrorMessage('Sorry, this command is unavailable to use in DMs.'));
     }
 
+    // Prevent the command from running if the user doesn't have enough permission
+    // Get the member's permission level
+    const lvlObject = this.client.permLevel(message);
+
+    // First we check to see if the level set in the command actually exists
+    if (!this.client.levelCache[command.conf.permLevel]) {
+      return message.channel.send(Utils.createErrorMessage('The command you\'re trying to run is improperly configured.'));
+    }
+
+    // Then we check that the user's own permission level is higher than the MINIMUM required by
+    // the command
     if (lvlObject.lvl < this.client.levelCache[command.conf.permLevel]) {
+      console.log(`PERM LEVEL: ${this.client.levelCache[command.conf.permLevel]}`);
       if (guildSettings.systemNotice) {
         return message.channel.send(Utils.createErrorMessage('You do not have enough permissions to use this command.'));
       }
       return;
     }
 
+    // Save the current user's permission level in the message.author object
     message.author.permLevel = lvlObject.lvl;
 
     // Finally we run the command

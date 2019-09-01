8429988616c8ba9aa4d894b154b91ad657c60d00
BalenD <balen1996@hotmail.com>
2019-09-01 20:54:59 +0200

    properly displays server icon format, added axios dependency

--- commands/basic/server.js
+++ commands/basic/server.js
@@ -1,4 +1,7 @@
+const { RichEmbed } = require('discord.js');
+const axios = require('axios');
 const Command = require('../../models/Command');
+
 
 class Server extends Command {
   constructor(client) {
@@ -10,9 +13,29 @@ class Server extends Command {
       guildOnly: true,
       permLevel: 'Member',
     });
-  }
-  async run(message) {
-    console.log(message);
+    this.run = async (message) => {
+      const { guild } = message;
+
+      const ServerInfo = new RichEmbed()
+        .setColor('#4287f5')
+        .setDescription('Viewing server information')
+        .addField('Name of the Server:', guild.name)
+        .addField('Owner of Server:', guild.owner)
+        .addField('Server creation Date:', guild.createdAt)
+        .addField('Server Region:', guild.region)
+        .addField('Members:', guild.memberCount);
+
+      if (guild.iconURL) {
+        const serverIconUrl = guild.iconURL.slice(0, -4);
+        const res = await axios.head(serverIconUrl);
+        const type = res.headers['content-type'];
+        const serverIconType = type.slice(type.indexOf('/') + 1);
+        console.log(serverIconUrl)
+        console.log(serverIconType)
+        ServerInfo.setThumbnail(`${serverIconUrl}.${serverIconType}`);
+      }
+      message.channel.send(ServerInfo);
+    };
   }
 }
 

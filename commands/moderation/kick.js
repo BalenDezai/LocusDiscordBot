6b7d182bc15d90f6c32f235f2178c0633f4b65b2
BalenD <balen1996@hotmail.com>
2019-09-01 02:14:57 +0200

    added kick command and fixed some issues in ban command

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -2,52 +2,41 @@ const discordApiError = require('discord.js').DiscordAPIError;
 const Command = require('../../models/Command');
 const Utils = require('../../modules/Utils');
 
-class Ban extends Command {
+
+class Kick extends Command {
   constructor(client) {
     super(client, {
-      name: 'ban',
-      description: 'Bans a mentioned user or users, plus you can specify the amount of days of messages to remove',
+      name: 'kick',
+      description: 'kicks a mentioned user',
       category: 'Moderation',
-      usage: ['ban [user mention/s]', ['ban [user mention/s] -d [days] -r [reason]']],
+      usage: ['kick [user mention/s]', 'kick [user mention/s] -r [reason]'],
       guildOnly: true,
       permLevel: 'Moderator',
     });
   }
 
   async run(message, args) {
-    //  find the indexes for arguements
-    const indexOfD = args.indexOf('-d');
     const indexOfR = args.indexOf('-r');
-    let days = 0;
     let reason = '';
     let displaySuccess;
 
-    if (indexOfD !== -1) {
-      //  parse the day arguement
-      days = parseInt(args.slice(indexOfD + 1, indexOfR), 10);
-    }
     if (indexOfR !== -1) {
-      //  seperate the reason into a whole string
       reason = args.slice(indexOfR + 1).join(' ');
     }
 
-    const bannedUsersTags = [];
+    const kickedUsersTags = [];
 
-    //  for each mentioned user, ban the user, inserting the days and reason
     if (message.mentions.users.size > 0) {
       message.mentions.users.forEach((user) => {
         const member = message.guild.member(user);
         if (member) {
-          member.ban({
-            reason,
-            days,
-          })
-            .then(() => bannedUsersTags.push(user.tag))
+          member.kick(reason)
+            .then(() => kickedUsersTags.push(user.tag))
             .catch((e) => {
               if (e instanceof discordApiError) {
                 message.channel.send(Utils.createErrorMessage(e.message));
               } else {
-                message.channel.send(Utils.createErrorMessage(`Failed to ban user: **${user.tag}**`));
+                message.channel.send(Utils.createErrorMessage(`Failed to kick user: **${user.tag}**`));
               }
               displaySuccess = false;
               this.client.logger.error(e);
@@ -57,12 +46,12 @@ class Ban extends Command {
         }
       });
       if (displaySuccess === true) {
-        message.channel.send(Utils.createSuccessMessage(`Successfully banned user(s) ${bannedUsersTags.join(' ')}`));
+        message.channel.send(Utils.createSuccessMessage(`Successfully kicked user(s) ${kickedUsersTags.join(' ')}`));
       }
     } else {
-      message.channel.send(Utils.createErrorMessage('You didn\'t mention any users to ban'));
+      message.channel.send(Utils.createErrorMessage('You didn\'t mention any users to kick'));
     }
   }
 }
 
-module.exports = Ban;
+module.exports = Kick;
